2f7cbc03c40a545011542f06d6c74ca5fbf48abf
Fayaz Shaikh <67692714+azadeutsche114@users.noreply.github.com>
2024-08-16 15:24:30 +0200

    Add files via upload

---
+++ Breast_Cancer_Detection_Using_Machine_Learning_Classifier.py
@@ -0,0 +1,649 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Breast Cancer Detection Using Machine Learning Classifier
+
+# by www.IndianAIProduction.com <br>
+# Project Source: www.IndianAIProduction.com/breast-cancer-detection-using-machine-learning-classifier <br>
+# ML Projects: www.IndianAIProduction.com/machine-learning-projects <br>
+# Videos: www.YouTube.com/IndianAIProduction
+
+# # Import essential libraries 
+
+# In[1]:
+
+
+# import libraries
+import pandas as pd # for data manupulation or analysis
+import numpy as np # for numeric calculation
+import matplotlib.pyplot as plt # for data visualization
+import seaborn as sns # for data visualization
+
+
+# # Data Load
+
+# In[2]:
+
+
+#Load breast cancer dataset
+cancer_dataset = r"E:\Projects\ML Projects\001 Bike-Sharing-Demand-Prediction\data\BreastcancerData.csv"
+
+
+# # Data Manupulation
+
+# In[3]:
+
+
+cancer_dataset
+
+
+# In[4]:
+
+
+type(cancer_dataset)
+
+
+# In[5]:
+
+
+# keys in dataset
+cancer_dataset.keys()
+
+
+# In[6]:
+
+
+# featurs of each cells in numeric format
+cancer_dataset['data']
+
+
+# In[7]:
+
+
+type(cancer_dataset['data'])
+
+
+# In[8]:
+
+
+# malignant or benign value
+cancer_dataset['target']
+
+
+# In[9]:
+
+
+# target value name malignant or benign tumor
+cancer_dataset['target_names']
+
+
+# In[10]:
+
+
+# description of data
+print(cancer_dataset['DESCR'])
+
+
+# In[11]:
+
+
+# name of features
+print(cancer_dataset['feature_names'])
+
+
+# In[12]:
+
+
+# location/path of data file
+print(cancer_dataset['filename'])
+
+
+# ## Create DataFrame
+
+# In[13]:
+
+
+# create datafrmae
+cancer_df = pd.DataFrame(np.c_[cancer_dataset['data'],cancer_dataset['target']],
+             columns = np.append(cancer_dataset['feature_names'], ['target']))
+
+
+# In[14]:
+
+
+# DataFrame to CSV file
+cancer_df.to_csv('breast_cancer_dataframe.csv')
+
+
+# In[15]:
+
+
+# Head of cancer DataFrame
+cancer_df.head(6) 
+
+
+# In[16]:
+
+
+# Tail of cancer DataFrame
+cancer_df.tail(6) 
+
+
+# In[17]:
+
+
+# Information of cancer Dataframe
+cancer_df.info()
+
+
+# In[18]:
+
+
+# Numerical distribution of data
+cancer_df.describe() 
+
+
+# In[19]:
+
+
+cancer_df.isnull().sum()
+
+
+# # Data Visualization
+
+# In[20]:
+
+
+# Paiplot of cancer dataframe
+sns.pairplot(cancer_df, hue = 'target') 
+
+
+# In[21]:
+
+
+# pair plot of sample feature
+sns.pairplot(cancer_df, hue = 'target', 
+             vars = ['mean radius', 'mean texture', 'mean perimeter', 'mean area', 'mean smoothness'] ) # ****** img 5 ***
+
+
+# In[22]:
+
+
+# Count the target class
+sns.countplot(cancer_df['target']) #  **************************** img 5 *************************  
+
+
+# In[23]:
+
+
+# counter plot of feature mean radius
+plt.figure(figsize = (20,8))
+sns.countplot(cancer_df['mean radius']) # *** img 7 ****
+
+
+# # Heatmap
+
+# In[24]:
+
+
+# heatmap of DataFrame
+plt.figure(figsize=(16,9))
+sns.heatmap(cancer_df) # **** img 8 ****
+
+
+# ##  Heatmap of a correlation matrix 
+
+# In[25]:
+
+
+cancer_df.corr()
+
+
+# In[26]:
+
+
+# Heatmap of Correlation matrix of breast cancer DataFrame
+plt.figure(figsize=(20,20))
+sns.heatmap(cancer_df.corr(), annot = True, cmap ='coolwarm', linewidths=2) # *** img 9 ***
+
+
+# # Correlation Barplot
+
+# In[27]:
+
+
+# create second DataFrame by droping target
+cancer_df2 = cancer_df.drop(['target'], axis = 1)
+print("The shape of 'cancer_df2' is : ", cancer_df2.shape)
+
+
+# In[28]:
+
+
+#cancer_df2.corrwith(cancer_df.target)
+
+
+# In[29]:
+
+
+# visualize correlation barplot
+plt.figure(figsize = (16,5))
+ax = sns.barplot(cancer_df2.corrwith(cancer_df.target).index, cancer_df2.corrwith(cancer_df.target))
+ax.tick_params(labelrotation = 90) # **** img 10 ***
+
+
+# In[30]:
+
+
+cancer_df2.corrwith(cancer_df.target).index
+
+
+# # Split DatFrame in Train and Test
+
+# In[31]:
+
+
+# input variable
+X = cancer_df.drop(['target'], axis = 1) 
+X.head(6)
+
+
+# In[32]:
+
+
+# output variable
+y = cancer_df['target'] 
+y.head(6)
+
+
+# In[33]:
+
+
+# split dataset into train and test
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 5)
+
+
+# In[34]:
+
+
+X_train
+
+
+# In[35]:
+
+
+X_test
+
+
+# In[36]:
+
+
+y_train
+
+
+# In[37]:
+
+
+y_test
+
+
+# # Feature scaling 
+
+# In[38]:
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train_sc = sc.fit_transform(X_train)
+X_test_sc = sc.transform(X_test)
+
+
+# # Machine Learning Model Building
+
+# In[39]:
+
+
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+
+
+# ## Suppor vector Classifier
+
+# In[40]:
+
+
+# Support vector classifier
+from sklearn.svm import SVC
+svc_classifier = SVC()
+svc_classifier.fit(X_train, y_train)
+y_pred_scv = svc_classifier.predict(X_test)
+accuracy_score(y_test, y_pred_scv)
+
+
+# In[41]:
+
+
+# Train with Standard scaled Data
+svc_classifier2 = SVC()
+svc_classifier2.fit(X_train_sc, y_train)
+y_pred_svc_sc = svc_classifier2.predict(X_test_sc)
+accuracy_score(y_test, y_pred_svc_sc)
+
+
+# # Logistic Regression
+
+# In[42]:
+
+
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression
+lr_classifier = LogisticRegression(random_state = 51, penalty = 'l1')
+lr_classifier.fit(X_train, y_train)
+y_pred_lr = lr_classifier.predict(X_test)
+accuracy_score(y_test, y_pred_lr)
+
+
+# In[43]:
+
+
+# Train with Standard scaled Data
+lr_classifier2 = LogisticRegression(random_state = 51, penalty = 'l1')
+lr_classifier2.fit(X_train_sc, y_train)
+y_pred_lr_sc = lr_classifier.predict(X_test_sc)
+accuracy_score(y_test, y_pred_lr_sc)
+
+
+# # K – Nearest Neighbor Classifier
+
+# In[44]:
+
+
+# K – Nearest Neighbor Classifier
+from sklearn.neighbors import KNeighborsClassifier
+knn_classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+knn_classifier.fit(X_train, y_train)
+y_pred_knn = knn_classifier.predict(X_test)
+accuracy_score(y_test, y_pred_knn)
+
+
+# In[45]:
+
+
+# Train with Standard scaled Data
+knn_classifier2 = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+knn_classifier2.fit(X_train_sc, y_train)
+y_pred_knn_sc = knn_classifier.predict(X_test_sc)
+accuracy_score(y_test, y_pred_knn_sc)
+
+
+# # Naive Bayes Classifier
+
+# In[46]:
+
+
+# Naive Bayes Classifier
+from sklearn.naive_bayes import GaussianNB
+nb_classifier = GaussianNB()
+nb_classifier.fit(X_train, y_train)
+y_pred_nb = nb_classifier.predict(X_test)
+accuracy_score(y_test, y_pred_nb)
+
+
+# In[47]:
+
+
+# Train with Standard scaled Data
+nb_classifier2 = GaussianNB()
+nb_classifier2.fit(X_train_sc, y_train)
+y_pred_nb_sc = nb_classifier2.predict(X_test_sc)
+accuracy_score(y_test, y_pred_nb_sc)
+
+
+# # Decision Tree Classifier
+
+# In[48]:
+
+
+# Decision Tree Classifier
+from sklearn.tree import DecisionTreeClassifier
+dt_classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 51)
+dt_classifier.fit(X_train, y_train)
+y_pred_dt = dt_classifier.predict(X_test)
+accuracy_score(y_test, y_pred_dt)
+
+
+# In[49]:
+
+
+# Train with Standard scaled Data
+dt_classifier2 = DecisionTreeClassifier(criterion = 'entropy', random_state = 51)
+dt_classifier2.fit(X_train_sc, y_train)
+y_pred_dt_sc = dt_classifier.predict(X_test_sc)
+accuracy_score(y_test, y_pred_dt_sc)
+
+
+#  # Random Forest Classifier
+
+# In[50]:
+
+
+# Random Forest Classifier
+from sklearn.ensemble import RandomForestClassifier
+rf_classifier = RandomForestClassifier(n_estimators = 20, criterion = 'entropy', random_state = 51)
+rf_classifier.fit(X_train, y_train)
+y_pred_rf = rf_classifier.predict(X_test)
+accuracy_score(y_test, y_pred_rf)
+
+
+# In[51]:
+
+
+# Train with Standard scaled Data
+rf_classifier2 = RandomForestClassifier(n_estimators = 20, criterion = 'entropy', random_state = 51)
+rf_classifier2.fit(X_train_sc, y_train)
+y_pred_rf_sc = rf_classifier.predict(X_test_sc)
+accuracy_score(y_test, y_pred_rf_sc)
+
+
+# # AdaBoost Classifier
+
+# In[52]:
+
+
+# Adaboost Classifier
+from sklearn.ensemble import AdaBoostClassifier
+adb_classifier = AdaBoostClassifier(DecisionTreeClassifier(criterion = 'entropy', random_state = 200),
+                                    n_estimators=2000,
+                                    learning_rate=0.1,
+                                    algorithm='SAMME.R',
+                                    random_state=1,)
+adb_classifier.fit(X_train, y_train)
+y_pred_adb = adb_classifier.predict(X_test)
+accuracy_score(y_test, y_pred_adb)
+
+
+# In[53]:
+
+
+# Train with Standard scaled Data
+adb_classifier2 = AdaBoostClassifier(DecisionTreeClassifier(criterion = 'entropy', random_state = 200),
+                                    n_estimators=2000,
+                                    learning_rate=0.1,
+                                    algorithm='SAMME.R',
+                                    random_state=1,)
+adb_classifier2.fit(X_train_sc, y_train)
+y_pred_adb_sc = adb_classifier2.predict(X_test_sc)
+accuracy_score(y_test, y_pred_adb_sc)
+
+
+# # XGBoost Classifier
+
+# In[54]:
+
+
+# XGBoost Classifier
+from xgboost import XGBClassifier
+xgb_classifier = XGBClassifier()
+xgb_classifier.fit(X_train, y_train)
+y_pred_xgb = xgb_classifier.predict(X_test)
+accuracy_score(y_test, y_pred_xgb)
+
+
+# In[55]:
+
+
+# Train with Standard scaled Data
+xgb_classifier2 = XGBClassifier()
+xgb_classifier2.fit(X_train_sc, y_train)
+y_pred_xgb_sc = xgb_classifier2.predict(X_test_sc)
+accuracy_score(y_test, y_pred_xgb_sc)
+
+
+#  # XGBoost Parameter Tuning Randomized Search 
+
+# In[56]:
+
+
+params={
+ "learning_rate"    : [0.05, 0.10, 0.15, 0.20, 0.25, 0.30 ] ,
+ "max_depth"        : [ 3, 4, 5, 6, 8, 10, 12, 15],
+ "min_child_weight" : [ 1, 3, 5, 7 ],
+ "gamma"            : [ 0.0, 0.1, 0.2 , 0.3, 0.4 ],
+ "colsample_bytree" : [ 0.3, 0.4, 0.5 , 0.7 ] 
+}
+
+
+# In[57]:
+
+
+# Randomized Search
+from sklearn.model_selection import RandomizedSearchCV
+random_search = RandomizedSearchCV(xgb_classifier, param_distributions=params, scoring= 'roc_auc', n_jobs= -1, verbose= 3)
+random_search.fit(X_train, y_train)
+
+
+# In[58]:
+
+
+random_search.best_params_
+
+
+# In[59]:
+
+
+random_search.best_estimator_
+
+
+# In[60]:
+
+
+# training XGBoost classifier with best parameters
+xgb_classifier_pt = XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+       colsample_bynode=1, colsample_bytree=0.4, gamma=0.2,
+       learning_rate=0.1, max_delta_step=0, max_depth=15,
+       min_child_weight=1, missing=None, n_estimators=100, n_jobs=1,
+       nthread=None, objective='binary:logistic', random_state=0,
+       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None,
+       silent=None, subsample=1, verbosity=1)
+
+xgb_classifier_pt.fit(X_train, y_train)
+y_pred_xgb_pt = xgb_classifier_pt.predict(X_test)
+
+
+# In[61]:
+
+
+accuracy_score(y_test, y_pred_xgb_pt)
+
+
+# # Grid Search
+
+# In[62]:
+
+
+from sklearn.model_selection import GridSearchCV 
+grid_search = GridSearchCV(xgb_classifier, param_grid=params, scoring= 'roc_auc', n_jobs= -1, verbose= 3)
+grid_search.fit(X_train, y_train)
+
+
+# In[63]:
+
+
+grid_search.best_estimator_
+
+
+# In[64]:
+
+
+xgb_classifier_pt_gs = XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+       colsample_bynode=1, colsample_bytree=0.3, gamma=0.0,
+       learning_rate=0.3, max_delta_step=0, max_depth=3,
+       min_child_weight=1, missing=None, n_estimators=100, n_jobs=1,
+       nthread=None, objective='binary:logistic', random_state=0,
+       reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None,
+       silent=None, subsample=1, verbosity=1)
+xgb_classifier_pt_gs.fit(X_train, y_train)
+y_pred_xgb_pt_gs = xgb_classifier_pt_gs.predict(X_test)
+accuracy_score(y_test, y_pred_xgb_pt_gs)
+
+
+# # Confusion Matrix
+
+# In[65]:
+
+
+cm = confusion_matrix(y_test, y_pred_xgb_pt)
+plt.title('Heatmap of Confusion Matrix', fontsize = 15)
+sns.heatmap(cm, annot = True)
+plt.show()
+
+The model is giving 0 type II error and it is best
+# # Classification Report Of model
+
+# In[66]:
+
+
+print(classification_report(y_test, y_pred_xgb_pt))
+
+
+# # Cross-validation of the ML model
+
+# In[67]:
+
+
+# Cross validation
+from sklearn.model_selection import cross_val_score
+cross_validation = cross_val_score(estimator = xgb_classifier_pt, X = X_train_sc,y = y_train, cv = 10)
+print("Cross validation accuracy of XGBoost model = ", cross_validation)
+print("\nCross validation mean accuracy of XGBoost model = ", cross_validation.mean())
+
+
+# # Save XGBoost Classifier model using Pickel
+
+# In[68]:
+
+
+## Pickle
+import pickle
+
+# save model
+pickle.dump(xgb_classifier_pt, open('breast_cancer_detector.pickle', 'wb'))
+
+# load model
+breast_cancer_detector_model = pickle.load(open('breast_cancer_detector.pickle', 'rb'))
+
+# predict the output
+y_pred = breast_cancer_detector_model.predict(X_test)
+
+# confusion matrix
+print('Confusion matrix of XGBoost model: \n',confusion_matrix(y_test, y_pred),'\n')
+
+# show the accuracy
+print('Accuracy of XGBoost model = ',accuracy_score(y_test, y_pred))
+
+
+# End ==================================================    <br>
+# This Project Created By www.IndianAIProduction.com <br>
+# Project Source: www.IndianAIProduction.com/breast-cancer-detection-using-machine-learning-classifier <br>
+# ML Projects: www.IndianAIProduction.com/machine-learning-projects <br>
+# Videos: www.YouTube.com/IndianAIProduction
